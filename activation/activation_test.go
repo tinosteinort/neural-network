46e1b8b7baed0f86ed88ca302f6e1536a50b2650
Tino Steinort <tinosteinort@live.de>
2024-01-28 14:05:04 +0100

    fix and test sigmoid function

--- activation/activation_test.go
+++ activation/activation_test.go
@@ -15,43 +15,64 @@ var _ = Describe("Activation", func() {
 		Expect(activation.WithCustom([]network.Activation{})).NotTo(HaveOccurred())
 	})
 
-	It("should not find function", func() {
-		_, err := activation.ByName("does-not-exist")
-		Expect(err).To(Equal(fmt.Errorf("activation function not found: does-not-exist")))
-	})
+	Describe("byName", func() {
 
-	It("should find builtin function", func() {
-		f, err := activation.ByName("step")
-		Expect(f).NotTo(BeNil())
-		Expect(err).NotTo(HaveOccurred())
-		Expect(f.Name).To(Equal("step"))
-	})
-
-	It("should register and find custom function", func() {
-		Expect(activation.WithCustom([]network.Activation{
-			{
-				Name: "custom-implementation",
-				Run: func(input []float64, n network.Neuron) float64 {
-					return 0
-				},
-			},
-		})).NotTo(HaveOccurred())
-
-		f, err := activation.ByName("custom-implementation")
-		Expect(f).NotTo(BeNil())
-		Expect(err).NotTo(HaveOccurred())
-		Expect(f.Name).To(Equal("custom-implementation"))
-	})
-
-	It("should not be possible to override existing builtin function", func() {
-		err := activation.WithCustom([]network.Activation{
-			{
-				Name: "step",
-				Run: func(input []float64, n network.Neuron) float64 {
-					return 0
-				},
-			},
+		It("should not find function", func() {
+			_, err := activation.ByName("does-not-exist")
+			Expect(err).To(Equal(fmt.Errorf("activation function not found: does-not-exist")))
 		})
-		Expect(err).To(Equal(errors.New("cannot override builtin function: step")))
+
+		It("should find builtin function", func() {
+			f, err := activation.ByName("step")
+			Expect(f).NotTo(BeNil())
+			Expect(err).NotTo(HaveOccurred())
+			Expect(f.Name).To(Equal("step"))
+		})
+	})
+
+	Describe("register", func() {
+		It("should register and find custom function", func() {
+			Expect(activation.WithCustom([]network.Activation{
+				{
+					Name: "custom-implementation",
+					Run: func(input []float64, n network.Neuron) float64 {
+						return 0
+					},
+				},
+			})).NotTo(HaveOccurred())
+
+			f, err := activation.ByName("custom-implementation")
+			Expect(f).NotTo(BeNil())
+			Expect(err).NotTo(HaveOccurred())
+			Expect(f.Name).To(Equal("custom-implementation"))
+		})
+
+		It("should not be possible to override existing builtin function", func() {
+			err := activation.WithCustom([]network.Activation{
+				{
+					Name: "step",
+					Run: func(input []float64, n network.Neuron) float64 {
+						return 0
+					},
+				},
+			})
+			Expect(err).To(Equal(errors.New("cannot override builtin function: step")))
+		})
+	})
+
+	Describe("sigmoid", func() {
+		It("should return correct value", func() {
+			f, err := activation.ByName("sigmoid")
+			Expect(err).NotTo(HaveOccurred())
+
+			v := f.Run(
+				[]float64{0.5, 0.8},
+				network.Neuron{
+					Threshold: 0.3,
+					Weights:   []float64{-0.2, 1.2},
+				},
+			)
+			Expect(v).To(Equal(0.6364525402815664))
+		})
 	})
 })

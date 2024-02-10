2c618712c8603e05e12e75b73b79d084bc4d8ba7
Tino Steinort <tinosteinort@live.de>
2024-02-10 22:40:10 +0100

    read records from file

--- dataset/file.go
+++ dataset/file.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"errors"
 	"os"
+	"strconv"
+	"strings"
 )
 
 type fileDataSet struct {
@@ -30,11 +32,51 @@ func (ds *fileDataSet) Next() (*Record, error) {
 
 	hasNext := ds.scanner.Scan()
 	if !hasNext {
-		return nil, nil
+		return nil, errors.New("no records left in dataset")
 	}
-	ds.scanner.Text()
+	l := ds.scanner.Text()
 
-	return nil, nil
+	return asRecord(&l)
+}
+
+func asRecord(s *string) (*Record, error) {
+	input, result, err := parseLine(s)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Record{
+		Input:  input,
+		Result: result,
+	}, nil
+}
+
+func parseLine(s *string) (input []float64, result []float64, err error) {
+	segments := strings.Split(*s, ";")
+
+	input, err = asFloats(&segments[0])
+	if err != nil {
+		return nil, nil, err
+	}
+
+	result, err = asFloats(&segments[1])
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return input, result, nil
+}
+
+func asFloats(s *string) ([]float64, error) {
+	var floats []float64
+	for _, v := range strings.Split(*s, ",") {
+		f, err := strconv.ParseFloat(v, 64)
+		if err != nil {
+			return nil, err
+		}
+		floats = append(floats, f)
+	}
+	return floats, nil
 }
 
 func (ds *fileDataSet) Close() error {
